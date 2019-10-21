a51048b38d951434ff94bd293dcf974f47beab12
Samson Okaludo <43629180+Samson-Ludo@users.noreply.github.com>
2019-10-21 21:59:14 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Calculator from './containers/Calculator';
+import './containers/Calculator.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
---
+++ src/NOTES.md
@@ -0,0 +1,19 @@
+#Calculator Display
+
+#Calculator Keypad
+
+##Functional key
+	-clear
+	-sign
+	-percent
+
+##Digit Key
+	-number
+	-dot
+
+##Operator Key
+	-divide
+	-multiply
+	-add
+	-minus
+	-equals
